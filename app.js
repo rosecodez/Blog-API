1802ab0ddd3129ecb7b0bfb06deb7a42f84687bf
Rose <rosebeats09@gmail.com>
2024-06-21 20:05:20 -0400

    Create endpoints code for posts

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const createError = require("http-errors");
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 const passport = require("passport");
 require("dotenv").config();
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-var app = express();
+const app = express();
+
+app.use(cors());
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
--- controllers/postController.js
+++ controllers/postController.js
@@ -1,1 +1,70 @@
 const Post = require("../models/post");
+const User = require("../models/user");
+const router = express.Router();
+
+let posts = [
+  {
+    id: 1,
+    title: "How I built my first website",
+    timestamp: new Date(),
+    published: true,
+    user: new User(),
+  },
+  {
+    id: 2,
+    title: "How I started coding",
+    timestamp: new Date(),
+    published: true,
+    user: new User(),
+  },
+];
+
+// GET all posts
+router.get("/", (req, res) => {
+  res.json(posts);
+});
+
+// Get a specific post by id
+router.get("/:id", (req, res) => {
+  const post = posts.find((post) => post.id === id);
+  if (!post) {
+    return res.status(404).json({ message: "Post not found" });
+  }
+  res.json(post);
+});
+
+// POST create a new post
+router.post("/", (req, res) => {
+  const newPost = {
+    id: posts.length + 1,
+    title: title.req.body,
+    timestamp: new Date(),
+    published: true,
+    user: new User(),
+  };
+  posts.push(newPost);
+  res.status(201).json(newPost);
+});
+
+// PUT update post by id
+router.put("/:id", (req, res) => {
+  const post = posts.find((post) => post.id === id);
+  if (!post) {
+    return res.status(404).json({ message: "Post not found" });
+  }
+  post.title = title;
+  post.content = content;
+  res.json(post);
+});
+
+// DELETE post by id
+router.delete("/:id", (req, res) => {
+  const index = posts.findIndex((post) => post.id === id);
+  if (!index) {
+    return res.status(404).json({ message: "Post not found" });
+  }
+  posts.splice(index, 1);
+  res.sendStatus(204);
+});
+
+module.exports = router;
